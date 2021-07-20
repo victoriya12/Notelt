b632029444f874bac2652ecd921bb97ee5813ae6
Denis-UX <deozero9@gmail.com>
2021-07-20 09:50:40 +0300

    DataBase

---
+++ app/src/main/java/com/example/notelt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.notelt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
